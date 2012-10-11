441267a0831229f2ee4cf59429d192b7bbc8e11a
OI <OI@OI-PC>
2012-10-11 10:46:09 +0700

    first commit

---
+++ src/human/emotion/detection/ImageProcessing/SegmentationSkin.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package human.emotion.detection.ImageProcessing;
+
+/**
+ *
+ * @author OI
+ */
+public class SegmentationSkin {
+    
+}
